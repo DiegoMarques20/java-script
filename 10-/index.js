75f5653bb610cc5e337ea76be87799cc9c1d34a5
DiegoMarques20 <108305338+DiegoMarques20@users.noreply.github.com>
2022-11-04 18:13:03 -0300

    diferença de VAR e LET

---
+++ 10-/index.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imprimindo Informações na Tela</title>
+</head>
+<body>
+
+    <h1>Imprimindo Informações na Tela (Terminal Prompt e Node)</h1>
+
+    <ul>
+        <li></li>
+    </ul>
+
+    <script>
+
+        (Informação é mostrada no terminal Node ou Prompt e não no navegador)
+
+        var programador = "Diego";
+        console.log(programador);
+
+
+    </script>
+    
+</body>
+</html>
