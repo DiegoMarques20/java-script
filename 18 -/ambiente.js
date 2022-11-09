6d239fbe360ee1408d71017b9c2c56eb0d583eb1
DiegoMarques20 <108305338+DiegoMarques20@users.noreply.github.com>
2022-11-09 11:31:40 -0300

    indexOf

--- 18 -/ambiente.js
+++ 18 -/ambiente.js
@@ -1,4 +1,4 @@
-let num = [4,,8,7,5]
+let num = [4,8,7,5]
 num.push(6)
 num.sort()
 
@@ -13,3 +13,7 @@ for ( let pos in num) {
 
     console.log(`A posição ${pos} tem o valor ${num[pos]}`)
 }
+
+let auto = ["ferrari","audi","BMW","Mercedes"]
+let pos = auto.indexOf("audi")
+console.log(`O carro audi está na posição ${pos}`)
