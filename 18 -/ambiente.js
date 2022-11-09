391da4b626bbf827488ea9254415f19bd6f8013f
DiegoMarques20 <108305338+DiegoMarques20@users.noreply.github.com>
2022-11-09 11:08:42 -0300

    add for in

--- 18 -/ambiente.js
+++ 18 -/ambiente.js
@@ -1,4 +1,15 @@
-let num = [4]
+let num = [4,,8,7,5]
 num.push(6)
+num.sort()
 
-console.log(`Nosso vetor é ${num}`)
+console.log(`Nosso vetor possui os seguintes números ${num}`)
+
+for ( let pos = 0 ; pos<num.length;pos++) {
+
+    console.log(num[pos])   
+} 
+
+for ( let pos in num) {
+
+    console.log(`A posição ${pos} tem o valor ${num[pos]}`)
+}
