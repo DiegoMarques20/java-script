b0df90d710e9efbcfe41e1061db4909a8c3c0fc6
DiegoMarques20 <108305338+DiegoMarques20@users.noreply.github.com>
2022-11-10 10:39:12 -0300

    add function

---
+++ 19-/ambiente.js
@@ -0,0 +1,10 @@
+let res = parouimpar(78)
+function parouimpar (n) {
+    if (n%2==0){
+        return"O número é Par"
+    } else {
+        return"O número é impar"
+    }
+    
+}
+console.log(res)
