eb43da39c8d8a2ff52e58fcd8ed8383d7c2572a0
DiegoMarques20 <108305338+DiegoMarques20@users.noreply.github.com>
2022-11-15 14:39:05 -0300

    add novos elementos

---
+++ 22 -/script.js
@@ -0,0 +1,8 @@
+function verificar() {
+
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('ano')
+    var res = document.getElementById('res')
+    
+}
