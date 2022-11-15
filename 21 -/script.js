af7e1fe4b58123a632878743e471fb2541c203ab
DiegoMarques20 <108305338+DiegoMarques20@users.noreply.github.com>
2022-11-15 12:41:18 -0300

    add fotos

--- 21 -/script.js
+++ 21 -/script.js
@@ -5,6 +5,22 @@ function carregar() {
  var data = new Date ()
  var hora = data.getHours()
  var minutos = data.getUTCMinutes()
- msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos`
+ msg.innerHTML = `Agora são <strong>${hora} horas</strong> e <strong>${minutos} minutos</strong>`
+
+ if (hora >= 0 && hora < 12) {
+
+    img.src = 'manha.png'
+
+ } else if (hora >= 12 && hora < 18) {
+
+     
+     img.src = 'tarde.png'
+
+ } else {
+
+    img.src = 'noite.png'
+
+ }
+
 }
 
