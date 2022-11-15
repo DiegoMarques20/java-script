c427b482d7c38735a3bb0266806e4c71c169e2df
DiegoMarques20 <108305338+DiegoMarques20@users.noreply.github.com>
2022-11-15 13:32:37 -0300

    atualizando informações

--- 21 -/script.js
+++ 21 -/script.js
@@ -3,22 +3,28 @@ function carregar() {
  var msg = window.document.getElementById('msg')
  var img = window.document.getElementById('imagem')
  var data = new Date ()
- var hora = data.getHours()
+var hora = data.getHours()
  var minutos = data.getUTCMinutes()
  msg.innerHTML = `Agora são <strong>${hora} horas</strong> e <strong>${minutos} minutos</strong>`
 
  if (hora >= 0 && hora < 12) {
 
-    img.src = 'manha.png'
+    img.src = 'imagens/manha1.png'
+
+    document.body.style.background = '#5BAAEB'
 
  } else if (hora >= 12 && hora < 18) {
 
      
-     img.src = 'tarde.png'
+     img.src = 'imagens/tarde1.png'
+
+     document.body.style.background = '#EBC796'
 
  } else {
 
-    img.src = 'noite.png'
+    img.src = 'imagens/noite1.png'
+
+    document.body.style.background = '#132F6B'
 
  }
 
