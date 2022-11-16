87b9b579713bf609ad5bf4a8520204bec3ffb6c0
DiegoMarques20 <108305338+DiegoMarques20@users.noreply.github.com>
2022-11-16 11:32:47 -0300

    add números

---
+++ 23 -/script.js
@@ -0,0 +1,34 @@
+function contar () {
+
+    let ini = document.getElementById('inicio')
+    let fim = document.getElementById('final') 
+    let inter = document.getElementById('intervalo')
+    let res = document.getElementById('res')
+
+     if(ini.value.length == 0 || fim.value.length == 0 || inter.value.length == 0) {
+
+        window.alert('[ERRO] Faltam dados a serem preenchidos!')
+
+     } else {
+            res.innerHTML = 'Contando...'
+
+            let i  = Number(ini.value)
+            let f  = Number(fim.value)
+            let p = Number(inter.value)
+            if (i < f) {
+                // Contagem crescente
+
+                for (let c = i; c <= f; c += p) {
+
+                    res.innerHTML += `${c} \u{1F449}`
+                  } 
+            } else {
+
+                for (let c = i; c >= f; c -= p) {
+                    res.innerHTML += `${c} \u{1F449}`
+                }
+
+            }
+            res.innerHTML += `\u{1F3C1}`
+     }
+}
